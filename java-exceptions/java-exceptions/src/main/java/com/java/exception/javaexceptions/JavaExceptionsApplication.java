50b86dad0f5c1c49ebfeb892234165c86e279522
vadiraj karanam <vadi.karnam@gmail.com>
2020-03-20 15:10:09 +0530

    basic project structure

---
+++ java-exceptions/java-exceptions/src/main/java/com/java/exception/javaexceptions/JavaExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.java.exception.javaexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExceptionsApplication.class, args);
+	}
+
+}
---
+++ java-exceptions/java-exceptions/src/test/java/com/java/exception/javaexceptions/JavaExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.exception.javaexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
